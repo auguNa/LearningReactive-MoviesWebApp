b41cc99688cff8abccda6fb69771aa45ef7825f3
auguNa <augustin.nae@gmail.com>
2024-08-15 10:55:44 +0200

    first commit

---
+++ movies/src/test/java/dev/augustin/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.augustin.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
